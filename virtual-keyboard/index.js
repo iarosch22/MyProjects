83721c8712c62fa53989c531718efd37cf3235d4
Vitaliy Yarosh <iarosch@protonmail.com>
2022-05-06 12:23:03 +0300

    fix: fixes classes func

--- virtual-keyboard/index.js
+++ virtual-keyboard/index.js
@@ -1,18 +1,33 @@
+const classes = ['key', 'key', 'key', 'key', 'key', 'key', 'key', 'key', 'key', 'key', 'key', 'key', 'key', 'double-key',
+                'double-key', 'key', 'key', 'key', 'key', 'key', 'key', 'key', 'key', 'key', 'key', 'key', 'key', 'key',
+                'double-key', 'key', 'key', 'key', 'key', 'key', 'key', 'key', 'key', 'key', 'key', 'key', 'double-key',
+                'double-key', 'key', 'key', 'key', 'key', 'key', 'key', 'key', 'key', 'key', 'key', 'key' ,'double-key',
+                'key', 'key', 'space', 'key', 'key', 'key', 'key', 'key'];
+
 const enKeys = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
                 `Tab`, 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\',
                 'Capslock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter',
                 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '&#8593;' ,'Shift',
                 'Ctrl', 'Alt', 'Space', 'Alt', '&#8592', '&#8595;', '&#8594;', 'Ctrl'];
 
-const ruKeys = [];
+const ruKeys = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
+                `Tab`, 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\',
+                'Capslock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter',
+                'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '&#8593;' ,'Shift',
+                'Ctrl', 'Alt', 'Space', 'Alt', '&#8592', '&#8595;', '&#8594;', 'Ctrl'];
+
+
 
 function сreateKeyboard() {
+    let lang = 'en';
+    let caps = false;
+    let shift = true;
+
     const BODY = document.querySelector('body');
 
     
     textarea = document.createElement('textarea');
     divKeyboard = document.createElement('div');
-   
     
 
     BODY.appendChild(textarea);
@@ -21,21 +36,25 @@ function сreateKeyboard() {
     divKeyboard.classList.add('keyboard');
 
     for(let i = 0; i < enKeys.length; i++) {
-        this.button = document.createElement('button');
-        this.button.innerHTML = enKeys[i];
-        this.button.classList.add('keyboard_key');
+        button = document.createElement('button');
+        button.innerHTML = getKeyValue(i);
+        button.classList.add(getKeyClass(i));
+        divKeyboard.appendChild(button);
+    }
 
-        if(enKeys[i] === 'Backspace' ||
-        enKeys[i] === 'Tab' ||
-        enKeys[i] === 'Capslock' ||
-        enKeys[i] === 'Enter' ||
-        enKeys[i] === 'Shift') {
-            this.button.classList.add('double-key');
+    function getKeyValue(index) {
+        let key = '';
+        if(lang === 'en') {
+            key = enKeys[index];
+        } else {
+            key = ruKeys[index];
         }
-        if(enKeys[i] === 'Space') {
-            this.button.classList.add('space');
-        }
-        divKeyboard.appendChild(this.button);
+
+        return key;
+    }
+
+    function getKeyClass(index) {
+        return classes[index];
     }
 }
 
